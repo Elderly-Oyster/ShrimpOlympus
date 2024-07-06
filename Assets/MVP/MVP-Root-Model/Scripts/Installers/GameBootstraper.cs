d88bf6a5e4bbe17e6fbfce26404b3f9e7de2ab0d
PinkM0th <98178122+PinkM0th@users.noreply.github.com>
2024-07-07 00:14:18 +0200

    Update module system with scene management

---
+++ Assets/MVP/MVP-Root-Model/Scripts/Installers/GameBootstraper.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GameBootstraper : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
---
+++ Assets/MVP/MVP-Root-Model/Scripts/Installers/StartLifetimeScope.cs
@@ -0,0 +1,25 @@
+﻿using MVP.MVP_Root_Model.Scripts.Core.Views;
+using MVP.MVP_Root_Model.Scripts.Modules.StartGame.Scripts;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace MVP.MVP_Root_Model.Scripts.Startup
+{
+    public class StartLifeTimeScope : LifetimeScope
+    {
+        [SerializeField] private RootCanvas rootCanvas;
+        [SerializeField] private Camera mainCamera;
+        [SerializeField] private StartGameScreenView startGameScreenView;
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterComponent(rootCanvas);
+            builder.RegisterInstance(mainCamera);
+
+            builder.RegisterInstance(startGameScreenView).As<StartGameScreenView>();
+            builder.Register<StartGameScreenPresenter>(Lifetime.Singleton);
+            builder.Register<StartGameScreenModel>(Lifetime.Singleton);
+        }
+    }
+}
