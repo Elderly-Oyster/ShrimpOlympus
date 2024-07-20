31f1d7f608616c55c5daf411b82bf1867a38fe1d
GIDROKS <molocokirill@gmail.com>
2024-07-20 06:07:24 +0300

    Add NetworkManager

---
+++ Assets/MVP/MVP-Root-Model/Scripts/Core/NetworkManager/Scripts/OfflinePopup.cs
@@ -0,0 +1,6 @@
+using MVP.MVP_Root_Model.Scripts.Core.Popup;
+
+namespace MVP.MVP_Root_Model.Scripts.Core.NetworkManager.Scripts
+{
+    public class OfflinePopup : BasePopup { }
+}
---
+++ Assets/MVP/MVP-Root-Model/Scripts/Core/Popup/PopupHub.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace MVP.MVP_Root_Model.Scripts.Core.Popup
+{
+    public class PopupHub : MonoBehaviour
+    {
+
+    }
+}
