2820ebcf8b09e45769657bc62fec193f71aaaa74
GIDROKS <molocokirill@gmail.com>
2024-09-09 12:46:16 +0300

    Add Custom UniRxExtensions

---
+++ Assets/Scripts/Core/UniRxExtensions/UniRxExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using TMPro;
+using UniRx;
+using UnityEngine.Events;
+
+namespace Core.UniRxExtensions
+{
+    public static class UniRxExtensions
+    {
+        public static IObservable<string> OnValueChangedAsObservable(this TMP_InputField inputField)
+        {
+            return Observable.FromEvent<UnityAction<string>, string>(
+                h => new UnityAction<string>(h),
+                h => inputField.onValueChanged.AddListener(h),
+                h => inputField.onValueChanged.RemoveListener(h)
+            );
+        }
+
+        public static IObservable<int> OnValueChangedAsObservable(this TMP_Dropdown dropdown)
+        {
+            return Observable.FromEvent<UnityAction<int>, int>(
+                h => new UnityAction<int>(h),
+                h => dropdown.onValueChanged.AddListener(h),
+                h => dropdown.onValueChanged.RemoveListener(h)
+            );
+        }
+    }
+}
