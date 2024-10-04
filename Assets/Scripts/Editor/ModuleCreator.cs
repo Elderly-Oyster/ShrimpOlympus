fba491185d191cc9fc5503ddee8209c4072a25fa
GIDROKS <molocokirill@gmail.com>
2024-10-04 03:48:57 +0300

    Update ModuleleCreator By Prefab Creation Toggle

--- Assets/Scripts/Editor/ModuleCreator.cs
+++ Assets/Scripts/Editor/ModuleCreator.cs
@@ -8,6 +8,7 @@ namespace Editor
     public class ModuleCreator : EditorWindow
     {
         private FolderType _selectedFolder = FolderType.Base;
+        private bool _createPrefab = true;
 
         public enum FolderType { Additional, Base, Test }
         private const float GUISpacing = 10f;
@@ -27,42 +28,20 @@ namespace Editor
         private void OnGUI()
         {
             GUILayout.Label("Module Creator", EditorStyles.boldLabel);
-
-            DrawModuleNameField();
-            GUILayout.Space(GUISpacing);
-
-            DrawFolderSelection();
-            GUILayout.Space(GUISpacing);
-
-            DrawScriptOptions();
-            GUILayout.Space(GUISpacing);
-
-            DrawAsmdefOption();
-            GUILayout.Space(GUISpacing);
-
-            HandleCreateModuleButton();
-        }
-
-        private void DrawModuleNameField() =>
             _moduleName = EditorGUILayout.TextField("Module Name", _moduleName);
-
-        private void DrawFolderSelection() =>
+            GUILayout.Space(GUISpacing);
             _selectedFolder = (FolderType)EditorGUILayout.EnumPopup("Select Folder", _selectedFolder);
-
-        private void DrawScriptOptions()
-        {
+            GUILayout.Space(GUISpacing);
             GUILayout.Label("Select Scripts to Create", EditorStyles.boldLabel);
             _createInstaller = EditorGUILayout.Toggle("Installer", _createInstaller);
             _createPresenter = EditorGUILayout.Toggle("Presenter", _createPresenter);
             _createView = EditorGUILayout.Toggle("View", _createView);
             _createModel = EditorGUILayout.Toggle("Model", _createModel);
-        }
-
-        private void DrawAsmdefOption() =>
+            GUILayout.Space(GUISpacing);
             _createAsmdef = EditorGUILayout.Toggle("Create asmdef", _createAsmdef);
-
-        private void HandleCreateModuleButton()
-        {
+            GUILayout.Space(GUISpacing);
+            _createPrefab = EditorGUILayout.Toggle("Create Prefab", _createPrefab);
+            GUILayout.Space(GUISpacing);
             if (GUILayout.Button("Create Module"))
             {
                 if (IsValidModuleName(_moduleName))
@@ -93,11 +72,14 @@ namespace Editor
             var targetModuleFolderPath = ModuleGenerator.
                 GetTargetModuleFolderPath(_moduleName, _selectedFolder.ToString());
 
-            var addPrefabTask = new AddPrefabTask(
-                _moduleName,
-                targetModuleFolderPath);
+            if (_createPrefab)
+            {
+                var addPrefabTask = new AddPrefabTask(
+                    _moduleName,
+                    targetModuleFolderPath);
 
-            TaskQueue.EnqueueTask(addPrefabTask);
+                TaskQueue.EnqueueTask(addPrefabTask);
+            }
 
             EditorUtility.DisplayDialog("Module Creation Started",
                 "Module creation process has started. Please wait for it to complete.", "OK");
