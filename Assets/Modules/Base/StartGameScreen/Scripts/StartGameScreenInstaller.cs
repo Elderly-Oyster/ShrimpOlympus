4d85cb05a8b684c453b3246e869a301ac20f7244
GIDROKS <molocokirill@gmail.com>
2025-05-04 16:13:34 +0300

    Update StartGameInstaller By BaseModuleSceneInstaller

--- Assets/Modules/Base/StartGameScreen/Scripts/StartGameScreenInstaller.cs
+++ Assets/Modules/Base/StartGameScreen/Scripts/StartGameScreenInstaller.cs
@@ -1,22 +1,16 @@
 ﻿using CodeBase.Core.Modules.Installer;
-using CodeBase.Core.UI;
-using CodeBase.Services.SceneInstallerService;
 using UnityEngine;
 using VContainer;
-using VContainer.Unity;
 
 namespace Modules.Base.StartGameScreen.Scripts
 {
-    public class StartGameScreenInstaller : SceneInstaller
+    public class StartGameScreenInstaller : BaseModuleSceneInstaller
     {
         [SerializeField] private StartGameView startGameView;
-        [SerializeField] private BaseScreenCanvas screenCanvas;
-        [SerializeField] private Camera mainCamera;
 
         public override void RegisterSceneDependencies(IContainerBuilder builder)
         {
-            builder.RegisterComponent(screenCanvas);
-            builder.RegisterInstance(mainCamera);
+            base.RegisterSceneDependencies(builder);
 
             builder.RegisterInstance(startGameView).As<StartGameView>();
             builder.Register<StartGameScreenPresenter>(Lifetime.Singleton);
