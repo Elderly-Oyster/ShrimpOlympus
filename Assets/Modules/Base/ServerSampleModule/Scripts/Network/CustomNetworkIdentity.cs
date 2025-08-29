5cacb08b46746d71806862773dfd2114237cdcbf
PinkM0th <bombardir0105@gmail.com>
2025-08-29 07:25:22 +0200

    Update CustomNetworkIdentity

--- Assets/Modules/Base/ServerSampleModule/Scripts/Network/CustomNetworkIdentity.cs
+++ Assets/Modules/Base/ServerSampleModule/Scripts/Network/CustomNetworkIdentity.cs
@@ -7,14 +7,24 @@ public class CustomNetworkIdentity : NetworkBehaviour
     [SerializeField] private Color color;
     [SerializeField] private Renderer renderer;
 
+    [SerializeField] private SyncVar<int> health = new(100);
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.A)) SetColor(color);
+        if (Input.GetKeyDown(KeyCode.S)) TakeDamage(10);
+    }
+
+    // [ObserversRpc(bufferLast: true)] to make new identities update 
+    [ObserversRpc]
+    private void SetColor(Color color1)
+    {   
+        renderer.material.color = color1;
     }
 
     [ServerRpc]
-    private void SetColor(Color color1)
+    private void TakeDamage(int damage)
     {
-        renderer.material.color = color1;
+        health.value -= damage;
     }
 }
